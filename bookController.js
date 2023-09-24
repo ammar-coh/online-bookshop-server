1e1665f5ae9b6c64f14311b888a29bf7ba55ad56
ammar-coh <ammarkhn91@gmail.com>
2023-09-24 11:08:46 +0500

    book delete api

--- bookController.js
+++ bookController.js
@@ -1,52 +1,73 @@
-const Product = require("./bookModel");
+const Book = require("./bookModel");
 //var mongoose = require('mongoose');
 const Auth = require("./auth");
 
 // Handle index actions
 
 exports.index = async (req, res) => {
-  const list = await Product.find();
+  const list = await Book.find();
   res.json(list);
 };
 // Handle create new product
 exports.new = async function (req, res) {
-  let { image, price, rating, stock, title, author,description,category } = req.body;
-  var product = new Product();
-  var currentList = await Product.find();
-  Object.assign(product, {
+  let { image, price, rating, stock, title, author, description, category } = req.body;
+  var book = new Book();
+  var currentList = await Book.find();
+  Object.assign(book, {
     image: image,
     price: price,
-    title:title, 
-    author:author,
+    title: title,
+    author: author,
     rating: rating,
     stock: stock,
-    description:description,
-    category:category
+    description: description,
+    category: category
   });
-  product.save();
-  currentList.push(product);
+  book.save();
+  currentList.push(book);
 
   var updatedList = currentList;
   res.json({
     message: "new product has been added!",
-    data: product,
-    updatedProductlist: updatedList,
+    data: book,
+    updatedBooklist: updatedList,
   });
 };
 
 // Handle update product info
 exports.updating = async function (req, res) {
-  const id = req.body.book_id;
+  const id = req.params.bookId;
   const updates = req.body;
   const options = { new: true };
-  const book = await Product.findByIdAndUpdate(id, updates, options);
+  const book = await Book.findByIdAndUpdate(id, updates, options);
   res.json(book);
 };
 // Handle delete product info
 exports.delete = async function (req, res) {
-  const id = req.body.book_id;
+  const id = await req.params.bookId;
+  try {
   const options = { new: true };
-  const deletedBook = await Product.findByIdAndDelete(id, options);
-  console.log(deletedBook);
-  res.json(deletedBook);
+  await Book.findByIdAndDelete(id, options);
+  const checkBookDeleted = await Book.findById({
+    _id: id,
+  });
+  var updatedBookList = await Book.find();
+ 
+    if (!checkBookDeleted) {
+      res.status(200).json({
+        status: true,
+        updatedBookList: updatedBookList
+      })
+    }
+    else {
+      res.status(400).json({
+        status: false,
+        updatedBookList: updatedBookList
+      })
+    }
+  }
+  catch (error) {
+    res.status(500).json({ error: 'An error occurred while removing the book.' });
+
+  }
 };
