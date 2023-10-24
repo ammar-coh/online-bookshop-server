2c549fde9b9dc8039058c10b325236defb2ed51f
ammar-coh <70238236+ammar-coh@users.noreply.github.com>
2023-10-24 08:56:24 +0500

    autocomplete search funcionality (#27)

--- models/bookModel.js
+++ models/bookModel.js
@@ -11,7 +11,6 @@ const book = mongoose.Schema({
   category:String
 });
 // book.index({ title: 1, author: 1 });
-book.index({ 'title': 'text', 'author': 'text'});
 var Book = (module.exports = mongoose.model("books", book));
 
 module.exports = Book;
