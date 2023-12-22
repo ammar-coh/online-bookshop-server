66686b4b6b1ffef91687c250cd736914da9b03e8
ammar-coh <ammarkhn91@gmail.com>
2023-12-22 18:15:29 +0500

    cloud update

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -23,8 +23,11 @@ exports.new = async function (req, res) {
     category: category
   });
   if (req.file) {
-    const newUrl = req.file ? req.file.path : '';
-    book.image = req.protocol + '://' + req.get('host') + '/' + newUrl.replace(/\\/g, '/'); 
+    // Handle the in-memory file differently
+    const buffer = req.file.buffer;
+    // Assuming you want to save the image as a Base64 encoded string
+    const base64Image = buffer.toString('base64');
+    updates.image = `data:${req.file.mimetype};base64,${base64Image}`;
   }
   book.save();
   currentList.push(book);
@@ -50,8 +53,11 @@ exports.updating = async function (req, res) {
       category,
     };
     if (req.file) {
-      const url = req.file ? req.file.path : '';
-      updates.image = req.protocol + '://' + req.get('host') + '/' + url.replace(/\\/g, '/'); // Add the filename of the uploaded image to the book data
+      // Handle the in-memory file differently
+      const buffer = req.file.buffer;
+      // Assuming you want to save the image as a Base64 encoded string
+      const base64Image = buffer.toString('base64');
+      updates.image = `data:${req.file.mimetype};base64,${base64Image}`;
     }
     const options = { new: true };
     const book = await Book.findByIdAndUpdate(id, updates, options);
